da1e656006d8c45d151c318f42441dbd10e38aee
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2025-12-08 15:53:58 -0300

    introdução a condições javascript

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.8
+
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+
+if(vel > 80){
+    console.log('velocidade acima do limite')
+}
+else{
+    console.log('velocidade permitida')
+}
