1a0fe7d506c2fe38fc7d927b2c10b55684d44aa7
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2025-12-09 15:06:40 -0300

    adiciona verificação de idade para votação

---
+++ aulas/aula12/ex011.js
@@ -0,0 +1,13 @@
+var idade = 25
+
+if(idade < 16){
+    console.log('nao vota')
+}
+else if (idade < 18 || idade > 70){
+        console.log('voto opcional')
+
+}
+
+else{
+    console.log('voto obrigatorio')
+}
