234f0a91ef98be34ca232bb6fb0a8692ef886040
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2026-01-20 15:45:03 -0300

    versão mais atual

--- aulas/aula15/vetores.js
+++ aulas/aula15/vetores.js
@@ -3,3 +3,10 @@ let vetor = [1, 2, 3, 4, 5, 6];
 for(let i = 0; i < vetor.length; i++){
     console.log(`a posição ${i} tem o valor ${vetor[i]}`);
 }
+console.log('\n');
+
+
+//versão mais atual e simples de demonstrar vetores
+for(i in vetor){
+    console.log(`a posição ${i} tem o valor ${vetor[i]}`);
+}
