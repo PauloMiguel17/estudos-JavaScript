45e1613fbd7529df565d05cdce993e66a2a91770
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2026-01-20 14:53:14 -0300

    função length

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -1,9 +1,13 @@
 let num = [5, 8, 9, 10];
 console.log(`nosso vetor é o ${num}`);
 
+//adicionar um valor ao vetor de forma manual
 num[4] = 15;
 console.log(`nosso vetor é o ${num}`);
 
-// de forma mais facil podemos usar a funçao push
+// de forma mais facil podemos usar a funçao push, adiciona no final 
 num.push(7);
 console.log(`nosso vetor é o ${num}`);
+
+//funçao para saber quantas posições tem o vetor
+console.log(`o vetor tem ${num.length} posiçoes`)
