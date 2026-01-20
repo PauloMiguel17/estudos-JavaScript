7dc23499c354cca3334624b4bf9da8dfd9b0f0fa
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2026-01-20 14:48:32 -0300

    função push

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -1,3 +1,9 @@
 let num = [5, 8, 9, 10];
+console.log(`nosso vetor é o ${num}`);
 
+num[4] = 15;
+console.log(`nosso vetor é o ${num}`);
+
+// de forma mais facil podemos usar a funçao push
+num.push(7);
 console.log(`nosso vetor é o ${num}`);
